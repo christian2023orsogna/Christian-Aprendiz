11bf14af515c9303f37747cde988ec3c88ec0cf9
christian2023orsogna <136644520+christian2023orsogna@users.noreply.github.com>
2023-06-14 17:34:16 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,8 +30,8 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Essentials for a cold winter'}
-        subtitle={'Discover Autumn Winter 2021'}
+        title={'Christian Aprendiz'}
+        subtitle={'Eduardo professor 2023'}
         ctaText={'shop now'}
         ctaAction={goToShop}
       />
